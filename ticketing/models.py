03c6fbaec9cd89530267207f34b166da09cc706e
Robo <a.abizadeh@gmail.com>
2021-05-26 13:36:08 +0430

    ShowTime model(for movie times) and db created

--- ticketing/models.py
+++ ticketing/models.py
@@ -24,3 +24,29 @@ class Cinema(models.Model):
     address = models.TextField()
     def __str__(self):
         return self.name
+class ShowTime(models.Model):
+    '''
+    creates show time of movies in cinema
+    '''
+    movie = models.ForeignKey('Movie',on_delete=models.PROTECT)
+    cinema = models.ForeignKey('Cinema',on_delete=models.PROTECT)
+    start_time = models.DateTimeField()
+    price = models.IntegerField()
+    salable_seats = models.IntegerField()
+    free_seats = models.IntegerField()
+
+    SALE_NOT_STARTED = 1
+    SALE_OPEN = 2
+    TICKETS_SOLD = 3
+    SALE_CLOSED = 4
+    MOVIE_PLAYED = 5
+    SHOW_CANCELED = 6
+    status_choice = (
+        (SALE_NOT_STARTED,'فروش آغاز نشده'),
+        (SALE_OPEN,'در حال فروش بلیط'),
+        (TICKETS_SOLD,'بلیت ها تمام شدند'),
+        (SALE_CLOSED,'فروش بلیت بسته شد'),
+        (MOVIE_PLAYED,'فیلم پخش شد'),
+        (SHOW_CANCELED,'سانس لغو شد'),
+    )
+    status = models.IntegerField(choices=status_choice)
