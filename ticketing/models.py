31ba1e9862b3fbd69441c50ae6a95cd6d5e110bf
Robo <a.abizadeh@gmail.com>
2021-05-26 13:42:25 +0430

    phone number in Cinema, from null changed to blank

--- ticketing/models.py
+++ ticketing/models.py
@@ -20,7 +20,7 @@ class Cinema(models.Model):
     name = models.CharField(max_length=50)
     city = models.CharField(max_length=30,default='تهران')
     capacity = models.IntegerField()
-    phone = models.CharField(max_length=20,null=True)
+    phone = models.CharField(max_length=20,blank=True)
     address = models.TextField()
     def __str__(self):
         return self.name
