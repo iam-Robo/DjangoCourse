d3d0470c97642bdb0686c07ff26fa099f51ede3b
Robo <a.abizadeh@gmail.com>
2021-05-30 21:46:49 +0430

    added comment

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from ticketing import views
 
-app_name = 'ticketing'
+app_name = 'ticketing' # this will be used for creating links in html files
 
 urlpatterns = [
     path('movie/list', views.movie_list,name='movie_list'),
