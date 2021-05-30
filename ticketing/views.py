29499ee3a2b1a690f3dbfb46c49c67cb060fe4ee
Robo <a.abizadeh@gmail.com>
2021-05-30 21:23:30 +0430

    created cinema_list.html , created director ticketing in template and moved movie_list.html , cinema_list.html to it

--- ticketing/views.py
+++ ticketing/views.py
@@ -8,22 +8,13 @@ from ticketing.models import Movie, Cinema
 def movie_list(request):
     movies = Movie.objects.all()
     context = {
-        'movie_list1' : movies
+        'movie_list1': movies
     }
-    return render(request,'movie_list.html',context)
+    return render(request, 'ticketing/movie_list.html', context)
 
 def cinema_list(request):
     cinemas = Cinema.objects.all()
-    response_text = """
-    <!DOCTYPE html>
-        <html dir=rtl>
-        <head>
-        <title>لیست سینماها</title>
-        </head>
-        <body style="background-color:#4d79ff;">
-        <h1 ><span style="color:#9400D3">فهرست سینماهای</span><span style="color: transparent;text-shadow: none;">&nbsp;</span><span style="color:#4B0082">کشور</span></h1>
-        <ul style="font-family:tahoma;color:#000099"> {} </ul>
-        </body>
-        </html>
-        """.format(''.join('<h4><li>{}</li></h4>'.format(item) for item in cinemas))
-    return HttpResponse(response_text)
+    context = {
+        'cinema_list1': cinemas
+    }
+    return render(request, 'ticketing/cinema_list.html', context)
