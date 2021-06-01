3e18e796f78ff6ff321a2a5bce182616ebec7411
Robo <a.abizadeh@gmail.com>
2021-06-01 16:31:41 +0430

    created url for cinema and movie details in urls.py , added functions of them to views.py. created cinema_details.html and movie_details.html

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from ticketing import views
 
-app_name = 'ticketing' # this will be used for creating links in html files
+app_name = 'ticketing' #this will be used for creating links in html files
 
 urlpatterns = [
-    path('movie/list', views.movie_list,name='movie_list'),
-    path('cinema/list', views.cinema_list,name='cinema_list')
+    path('movie/list/', views.movie_list, name='movie_list'),
+    path('movie/details/<int:movie_id>/', views.movie_details, name='movie_details'),
+    path('cinema/list/', views.cinema_list, name='cinema_list'),
+    path('cinema/details/<int:cinema_id>/', views.cinema_details, name='cinema_details')
 ]
--- ticketing/views.py
+++ ticketing/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from ticketing.models import Movie, Cinema
@@ -18,3 +18,17 @@ def cinema_list(request):
         'cinema_list1': cinemas
     }
     return render(request, 'ticketing/cinema_list.html', context)
+
+def movie_details(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    context = {
+        'movie': movie
+    }
+    return render(request, 'ticketing/movie_details.html', context)
+
+def cinema_details(request, cinema_id):
+    cinema = get_object_or_404(Cinema, pk=cinema_id)
+    context = {
+        'cinema': cinema
+    }
+    return render(request, 'ticketing/cinema_details.html', context)
