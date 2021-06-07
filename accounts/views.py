31a3ae1170535500f3133e9043b265a86663641f
Robo <a.abizadeh@gmail.com>
2021-06-07 19:38:05 +0430

    added codes to loging in user

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, Http404, HttpResponse
 
 # Create your views here.
 def login_view(request):
-    return render(request, 'accounts/login.html', {})
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            raise Http404('نام کاربری یا رمز اشتباه است')
+        else:
+            login(request,user)
+            return HttpResponse('ماشالاح')
+
+    else:
+        return render(request, 'accounts/login.html', {})
 
 def logout_view(request):
     pass
