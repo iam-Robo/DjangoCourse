0bf6914734861f48fda03888de362ff21a0c758e
Robo <a.abizadeh@gmail.com>
2021-06-21 22:11:46 +0430

    created model form

---
+++ accounts/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from accounts.models import Payment
+
+'''
+this is a model form , we use model form wheen we need to interact with database and models 
+otherwise we use normal forms
+'''
+
+class PaymentForm(forms.ModelForm):
+    class Meta:
+        model = Payment
+        fields = ['profile', 'amount', 'transaction_code']
