2bf1f1dce2736a96bb416dcb16a8bb25019eaf12
Robo <a.abizadeh@gmail.com>
2021-06-26 23:17:38 +0430

    modified validations to prevent error

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,15 +8,25 @@ this is a model form , we use model form when we need to interact with database 
 otherwise we use normal forms
 '''
 
+
 class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payment
         fields = ['amount', 'transaction_code']
 
+    # custom validations for form
 
-#custom validations for form
+    # the amount and amount in transaction code must be the same
+    def clean(self):
+        super().clean()
+        amount = self.cleaned_data.get('amount')
+        code = self.cleaned_data.get('transaction_code')
+        if code is not None and amount is not None:
+            code2 = re.findall('bank-(\d*)-\w*#', code)
+            if amount != int(code2[0]):
+                raise ValidationError('رسید و مبلغ تراکنش هم‌خوانی ندارند')
 
-    #transaction must be in 'bank-30000-UHB454GRH73BDYU#' format
+    # transaction must be in 'bank-30000-UHB454GRH73BDYU#' format
     def clean_transaction_code(self):
         code = self.cleaned_data.get('transaction_code')
         if not re.fullmatch('bank-\d*-\w*#', str(code)):
@@ -24,19 +34,10 @@ class PaymentForm(forms.ModelForm):
         else:
             return code
 
-    #amount must be devideable on 1000
+    # amount must be devideable on 1000
     def clean_amount(self):
         amount = self.cleaned_data.get('amount')
         if amount % 1000 != 0:
             raise ValidationError('مبلغ پرداختی باید مضربی از هزار باشد')
         else:
             return amount
-
-    #the amount and amount in transaction code must be the same
-    def clean(self):
-        super().clean()
-        amount = self.cleaned_data.get('amount')
-        code = self.cleaned_data.get('transaction_code')
-        code2 = re.findall('bank-(\d*)-\w*#', code)
-        if amount != int(code2[0]):
-            raise ValidationError('رسید و مبلغ تراکنش هم‌خوانی ندارند')
